6478675b2eb10ea3c9bfcacf318c6ab1a8be2308
unknown <yasmim.lima@dcomp.ufs.br>
2023-10-07 22:03:12 -0300

    tela de gerenciar temas

---
+++ Documents/Documents/SI Yas/7º Período/Programação Funcional/projeto-jogo-da-forca-main/projeto-jogo-da-forca-main/controller/forca.js
@@ -0,0 +1,50 @@
+import {name} from "./main"
+
+const selectedWord = name
+
+console.log(selectedWord)
+
+const indef = x => typeof x == 'undefined'
+
+const constructWord = (word, pos = null) => {
+    if(pos == null)
+        pos = 0; 
+    return word.length == 0 ? '' : `<td class="border box txt-center" id=${pos}></td>\n` + constructWord(word.slice(1), pos+1);
+}
+
+const renderWord = (elem) => {
+    const container = document.querySelector('.text tr')
+    container.innerHTML = elem
+}
+
+const verificarLetra = (letra,[x,...xs],acc=0) => {
+    if (indef(x)) return []
+    else if (letra == x) return [acc,...verificarLetra(letra,xs,acc+1)]
+    else return [...verificarLetra(letra,xs,acc+1)]
+}
+
+const insertWord = (id, word) => {
+    const box = document.getElementById(id)
+    box.innerHTML = word
+}
+
+const verifyExistance = (word) => {
+    const currentPosition = verificarLetra(word, selectedWord);
+
+    const iteratePosition = (list) => {
+        if(list.length == 0)  
+            return []
+
+        const currentWord = insertWord(list[0], word)
+
+        return iteratePosition(list.slice(1))
+    }
+
+
+    iteratePosition(currentPosition)
+    
+}
+
+const currentWord = constructWord(selectedWord)
+
+renderWord(currentWord);
