96aaae3502d3a1c4abc5d2b054eb6a6adc5e1538
Diego Carvaralho <139588340+Diiegocarva@users.noreply.github.com>
2023-10-09 00:59:27 -0300

    comentarios JavaScript-fundido.js

--- controller/JavaScript-fundido.js
+++ controller/JavaScript-fundido.js
@@ -1,4 +1,4 @@
-// elements
+// nomeando o id do html pra facilitar a manipulação
 const playBtn = document.getElementById('play-btn');
 const addBtn = document.getElementById('add-btn');
 const addWordBtn = document.getElementById('add-word-btn');
@@ -10,12 +10,11 @@ const confirmBtn = document.getElementById('btn-confirmar');
 const resultadoDiv = document.getElementById('resultado');
 
 
-
+//função que é utilizada ao clicar no botão de iniciar, sorteando uma palavra com math.random
 function playThemes() {
   const selectedTheme = themeSelect.value;
   const themesList = Object.keys(themes);
   if (selectedTheme === 'iniciar-jogo') {
-    // Escolher uma palavra aleatória com base no tema selecionado
     chosenWord = chooseRandomWord(themesList[Math.floor(Math.random() * themesList.length)]);
     resetGame();
     hideNonGameplayElements();
@@ -30,7 +29,7 @@ function playThemes() {
 }
 
 
-// events listeners
+// adiciona eventos, no caso a execução de funções ao clique
 playBtn.addEventListener('click', () => createGameBoard());
 addBtn.addEventListener('click', () => toggleAddSection());
 addWordBtn.addEventListener('click', () => addNewWord());
@@ -41,7 +40,7 @@ confirmBtn.addEventListener('click', () => playThemes());
 
 
 
-
+// função que inicializa o jogo, "cria o tabuleiro"
 function createGameBoard() {
     if (gameOver) keyboard.innerHTML = '';
     resetGame();
@@ -53,6 +52,8 @@ function createGameBoard() {
     drawLines();
 }
 
+//exclui qualquer texto de final de jogo, define a gameOver como false escolhe uma nova palavra com base no tema selecionado
+//redefine a palavra correta, a lista de letras e as tentativas restantes. basicamente reseta o jogo
 function resetGame() {
     deleteEndGameText();
     gameOver = false;
@@ -66,6 +67,7 @@ function resetGame() {
     tries = 10;
 }
 
+//mostra ou esconde a seção onde você pode adicionar novas palavras. Ela faz isso alternando a classe CSS 'hide' com classList.toggle ou .add ou .remove
 function toggleAddSection() {
     const addSection = document.getElementById('add-section');
     addSection.classList.toggle('hide');
@@ -74,6 +76,7 @@ function toggleAddSection() {
     selectThemeBtn.classList.toggle('hide');
 }
 
+//mesma coisa da função acima porém agora com a seção de seleção de temas
 function toggleThemeSection() {
     const addThemeSection = document.getElementById('add-theme-section');
     const bomDiaText = document.getElementById('bom-dia');
@@ -87,7 +90,8 @@ function toggleThemeSection() {
     selectThemeBtn.classList.add('hide');
 }
 
-
+//pega o valor digitado no campo de entrada e verifica se o comprimento da palavra está entre 4 e 8 caracteres. 
+//Se for uma palavra válida, ela verifica o tema selecionado no seletor e adiciona a palavra à lista de palavras desse tema
 function addNewWord() {
     const input = document.getElementById('new-word');
     const themeSelector = document.getElementById('theme-selector');
@@ -112,6 +116,7 @@ function addNewWord() {
     } else notifyInvalidWordInput();
 }
 
+//oculta elementos que não são necessários durante o jogo
 function hideNonGameplayElements() {
     const addBtn = document.getElementById('add-btn');
     const selectThemeBtn = document.getElementById('select-theme-btn');
@@ -125,6 +130,7 @@ function hideNonGameplayElements() {
     playBtn.classList.toggle('hide');
 }
 
+//exibe uma mensagem de notificação para informar ao jogador que a palavra que ele tentou adicionar é inválida
 function notifyInvalidWordInput() {
     const notice = document.getElementById('notice');
     notice.classList.add('invalid');
@@ -133,6 +139,7 @@ function notifyInvalidWordInput() {
     }, 2000);
 }
 
+//redefine o jogo, esconde o resultado do jogo anterior e exibe os botões da tela inicial
 function giveUpGame() {
     const body = document.getElementsByTagName("body")[0];
     const btnContainer = document.querySelectorAll(".buttons-container")[1];
@@ -150,18 +157,19 @@ function giveUpGame() {
     themeSelect.selectedIndex = 0;
 }
 
-// elements
+// elementos para "desenhar"
 const canvas = document.querySelector('canvas');
 const pencil = canvas.getContext("2d");
-const keyboard = document.getElementById('keyboard');
-const textStatus = document.getElementById("game-status");
-// variables
+const keyboard = document.getElementById('keyboard'); //responsavel pelo teclado
+const textStatus = document.getElementById("game-status"); // exibe vitória ou derrota
+
 const keys = [
   ['Q', 'W', 'E', 'R', 'T', 'Y', 'U', 'I', 'O', 'P'],
   ['A', 'S', 'D', 'F', 'G', 'H', 'J', 'K', 'L'],
   ['Z', 'X', 'C', 'V', 'B', 'N', 'M']
 ];
 
+//é uma função que ajusta o tamanho do elemento <canvas> com base na largura da janela do navegador, para exibir corretamente em diferentes tamanhos de tela
 function resizeCanvas() {
     if (window.innerWidth > 1366) {
         canvas.width = 1200;
@@ -179,6 +187,7 @@ function resizeCanvas() {
     pencil.strokeStyle = "#dcdcdc";
 }
 
+//é uma função que desenha o enforcado na tela com base no número de tentativas restantes 
 function drawHangman() {
     switch (tries) {
         case 9:
@@ -234,6 +243,7 @@ function drawHangman() {
     }
 }
 
+//desenha as linhas representando as letras da palavra
 function drawLines() {
     let axis = (canvas.width / 2) / chosenWord.length;
     let xPosition = (canvas.width / 2) - ((chosenWord.length / 2) * axis);
@@ -250,6 +260,7 @@ function drawLines() {
     pencil.closePath();
 }
 
+//desenha a letra corretamente advinhada na tela
 function drawCorrectLetter(i) {
     pencil.fillStyle = "#dcdcdc";
     pencil.font = "bold 2rem Poppins";
@@ -261,19 +272,21 @@ function drawCorrectLetter(i) {
     pencil.stroke();
 }
 
-
+//é uma função que exibe o resultado do jogo na tela, e exibe a palavra correta que o jogador deveria adivinhar caso ele perca
 function showEndGameText(status, text, correctWord) {
     textStatus.classList.add(status);
     textStatus.textContent = text + (correctWord ? ` A palavra era: ${correctWord}` : "");
     textStatus.classList.add('end-game-text');
 }
 
+//remove o texto de resultado do jogo da tela
 function deleteEndGameText() {
     textStatus.classList.remove('win');
     textStatus.classList.remove('lose');
     textStatus.textContent = "";
 }
 
+//cria o teclado virtual na tela com base nas letras definidas na matriz keys
 function createVirtualKeyboard() {
     if (keyboard.innerHTML === '') {
         for (let i = 0; i < keys.length; i++) {
@@ -289,6 +302,7 @@ function createVirtualKeyboard() {
     }
 }
 
+// mexe no layout da página quando o jogo se inicia, escondendo botões como o de adicionar palavras e mostrando o de desistir
 function rearrangeButtons() {
     const body = document.getElementsByTagName("body")[0];
     const btnContainer = document.querySelectorAll(".buttons-container")[1];
@@ -298,6 +312,7 @@ function rearrangeButtons() {
     giveUpBtn.classList.remove("hide");
 }
 
+// a matriz words contém uma lista de palavras que podem ser usadas no jogo
 let words = JSON.parse(localStorage.getItem('words')) || [
   'teclado', 'abacaxi', 'copo', 'caneta', 'bolo', 'quadrado', 'adesivo', 'marcador',
   'doce', 'folha', 'floresta', 'banco', 'cabelo', 'vermelho', 'desafio', 'porta',
@@ -307,6 +322,7 @@ let words = JSON.parse(localStorage.getItem('words')) || [
   'arroz', 'oceano'
 ];
 
+// objeto themes contém temas e cada tema tem uma matriz de palavras associadas a ele
 const themes = JSON.parse(localStorage.getItem('themes')) || {
   esporte: ['futebol', 'basquete', 'natacao', 'volei', 'corrida'],
   audiovisual: ['filme', 'série', 'atores', 'diretor', 'cinema'],
@@ -320,13 +336,14 @@ const themes = JSON.parse(localStorage.getItem('themes')) || {
 ],
 };
 
+// variaveis usadas para entender o andamento do jogo
+let chosenWord = ""; // armazena a palavra da vez
+let correctWord = ""; // armazena as letras corretas
+let letters = []; // armazena as letras já chutadas
+let tries = 10; // número de tentativas
+let gameOver = false; // indica se o jogo terminou
 
-let chosenWord = "";
-let correctWord = "";
-let letters = [];
-let tries = 10;
-let gameOver = false;
-
+//escolhe aleatoriamente uma palavra com base no tema selecionado
 function chooseRandomWord(theme) {
   const themeWords = themes[theme];
   if (themeWords && themeWords.length > 0) {
@@ -336,6 +353,8 @@ function chooseRandomWord(theme) {
   }
 }
 
+//verifica se o jogo não terminou, se a letra ainda não foi adivinhada
+//e então atualiza o estado do jogo com base na adivinhação do jogador
 function checkLetter(letter) {
     if (!gameOver) {
         if (!letters.includes(letter)) {
@@ -358,15 +377,17 @@ function checkLetter(letter) {
     }
 }
 
-
+//verifica as tentativas 
+// se número de tentativas chegar a zero, o jogador perdeu o jogo, e a função showEndGameText() é chamada para exibir a mensagem de derrota
+// se todas as letras corretas foram adivinhadas, o jogador venceu o jogo, e a função showEndGameText() é chamada para exibir a mensagem de vitória
 function checkGameOver() {
     if (tries == 0) {
-        // gameover
+
         showEndGameText("lose", "Fim de Jogo! Você perdeu!", chosenWord);
         gameOver = true;
     }
     if (correctWord.length === chosenWord.length) {
-        // gamewin
+
         showEndGameText("win", "Parabéns! Você venceu!");
         gameOver = true;
     }
@@ -374,7 +395,6 @@ function checkGameOver() {
 
 function addCorrectLetter(i) {
     correctWord += chosenWord[i].toUpperCase();
-    // Adicione uma classe CSS para indicar que a letra foi adivinhada corretamente
     const keysButtons = document.querySelectorAll('.letter');
     for (let j = 0; j < keysButtons.length; j++) {
         if (keysButtons[j].textContent === chosenWord[i].toUpperCase()) {
@@ -386,9 +406,8 @@ function addCorrectLetter(i) {
     }
 }
 
-
+// é usada para desativar visualmente uma tecla (botão) no teclado virtual
 function disableKey(key, status) {
-    //Add respective class to visually disable key
     const keysButtons = document.querySelectorAll('.letter');
     for (let i = 0; i < keysButtons.length; i++) {
         if (keysButtons[i].textContent === key) {
@@ -398,17 +417,18 @@ function disableKey(key, status) {
     }
 }
 
+// adiciona os eventos de clique ao teclado normal
 function addListeners() {
-    //Adds event listeners to keyboard
+
     window.addEventListener('keydown', (e) => {
         let letter = e.key.toUpperCase();
-        // validation if it's a letter
+
         if (keys.toString().includes(letter)) {
             checkLetter(letter);
         }
     });
 
-    //Adds events listeners to virtual keyboard
+    // adiciona os eventos de clique ao teclado virtual
     const keysButtons = document.querySelectorAll('.letter');
     keysButtons.forEach(key => key.addEventListener('click', () => {
         let letter = key.textContent;
